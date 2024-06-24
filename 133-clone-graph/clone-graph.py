6f70fd19a3829d57c956524a55d661934f8dfa07
CHAITANYA VARMA RUDRARAJU <140299567+Chaitu5210@users.noreply.github.com>
2024-06-24 19:52:05 +0530

    Time: 38 ms (74.43%) | Memory: 16.8 MB (67.16%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,22 +1,23 @@
-from typing import Optional
-
+"""
+# Definition for a Node.
 class Node:
-    def __init__(self, val=0, neighbors=None):
+    def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
+"""
 
+from typing import Optional
 class Solution:
-    def cloneGraph(self, node: Optional[Node]) -> Optional[Node]:
-        if not node:
-            return None
-        clone_dict = {}
-        def dfs(original_node):
-            if original_node.val in clone_dict:
-                return clone_dict[original_node.val]
-            clone = Node(original_node.val)
-            clone_dict[original_node.val] = clone
-            for neighbor in original_node.neighbors:
-                clone.neighbors.append(dfs(neighbor))
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        cloneDict={}
+        def dfs(node):
+            if node in cloneDict:
+                return cloneDict[node]
+            clone=Node(node.val)
+            cloneDict[node]=clone
+            for nei in node.neighbors:
+                clone.neighbors.append(dfs(nei))
             return clone
-        return dfs(node)
-                    
+
+        return dfs(node) if node else None
+        
