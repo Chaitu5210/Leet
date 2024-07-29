556f967aafa387d8c900845fc79295b6355135bf
Chaitanya-Varma-Rudraraju <140299567+Chaitu5210@users.noreply.github.com>
2024-07-29 14:42:55 +0530

    Time: 621 ms (33.34%) | Memory: 16.9 MB (17.19%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        result=0
+        def checklesserthan(leftlist,element,rightlist):
+            if len(leftlist)==0 or len(rightlist)==0:
+                return False
+            leftcounter=0
+            rightcounter=0
+            for i in range(len(leftlist)):
+                if element > leftlist[i]:
+                    leftcounter=leftcounter+1
+            for i in range(len(rightlist)):
+                if element < rightlist[i]:
+                    rightcounter=rightcounter+1
+            print(leftcounter,rightcounter,element)
+            return leftcounter*rightcounter
+
+        for i in range(len(rating)):
+            result=result+checklesserthan(rating[:i],rating[i],rating[i+1:])
+            result=result+checklesserthan(rating[i+1:],rating[i],rating[:i])
+        return result
+        
