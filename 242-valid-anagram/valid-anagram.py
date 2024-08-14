facf926513131a86670e9d2ea27af7cb13c1963b
Chaitanya-Varma-Rudraraju <140299567+Chaitu5210@users.noreply.github.com>
2024-08-14 08:39:40 +0530

    Time: 924 ms (5.17%) | Memory: 17.7 MB (7.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,9 +2,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         list1=list(s)
         list2=list(t)
-        list1.sort()
-        list2.sort()
-        if list1==list2:
-            return True
-        return False
+        if len(list1)!=len(list2):
+            return False
+        for element in list1:
+            if element in list2:
+                list2.remove(element)
+            else:
+                return False
+        return True
         
