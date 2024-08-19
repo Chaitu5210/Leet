cdfe663c9fc2ec6366f4de7bd9e632c1d36812d2
Chaitanya-Varma-Rudraraju <140299567+Chaitu5210@users.noreply.github.com>
2024-08-19 18:20:17 +0530

    Time: 288 ms (14.09%) | Memory: 26.7 MB (6.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,18 +1,29 @@
-from typing import List
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        result = [1] * n
-        prefix_product = 1
-        for i in range(n):
-            result[i] *= prefix_product
-            prefix_product *= nums[i]
-
-        # Calculate suffix products
-        suffix_product = 1
-        for i in range(n - 1, -1, -1):
-            result[i] *= suffix_product
-            suffix_product *= nums[i]
+        temp=1
+        leftside=[]
+        rightside=[]
+        for i in range(len(nums)):
+            leftside.append(temp)
+            temp=temp*nums[i]
+        print(leftside)
+        temp=1
+        print("finally ",temp)
+        i=len(nums)-1
+        while i>=0:
+            rightside.append(temp)
+            temp=temp*nums[i]
+            i=i-1
+        rightside.reverse()
+        result=[1]*len(nums)
+        print("leftside ",leftside)
+        print("rightside ",rightside)
+        for i in range(len(nums)):
+            result[i]=leftside[i]*rightside[i]
+        print(result)
         return result
-        
+
+            
+
+
+                    
