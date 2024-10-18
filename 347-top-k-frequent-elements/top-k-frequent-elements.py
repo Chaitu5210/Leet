e18a215d4b9e3dfb8329f7c82505fb00e543b1f5
Chaitanya-Varma-Rudraraju <140299567+Chaitu5210@users.noreply.github.com>
2024-10-18 08:05:35 +0530

    Time: 2 ms (100.00%) | Memory: 20 MB (99.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,14 +6,10 @@ class Solution:
                 Dict[element]=0
             Dict[element]+=1
         Desc_Dict=dict(sorted(Dict.items(),key=lambda items:items[1], reverse=True))
-        print(Dict)
-        print(Desc_Dict)
         result=[]
 
         for index,values in Desc_Dict.items():
-            print("k is ",k)
             if k>0:
-                print("Inside with k",k)
                 result.append(index)
             k=k-1
         return result
