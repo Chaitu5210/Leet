463c101687c1f4de9eece788547b415c139af497
Chaitanya-Varma-Rudraraju <140299567+Chaitu5210@users.noreply.github.com>
2024-11-26 08:51:37 +0530

    Time: 244 ms (5.30%) | Memory: 18.1 MB (9.65%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        if len(edges)==0 and n==1:
+            return 0
+        if len(edges)==0 and n>1:
+            return -1
+
+        edges_present = []
+        for edge in edges:
+            edges_present.append(edge[0])
+            edges_present.append(edge[1])
+        
+        edges_set=set(edges_present)
+        if len(edges_set) != n:
+            return -1
+
+
+
+
+
+
+
+
+        Dominating_end = []
+        Loosing_end = []
+
+        for edge in edges:
+            if edge[0] in Loosing_end:
+                Loosing_end.append(edge[1])
+            else:
+                if edge[0] not in Dominating_end:
+                    Dominating_end.append(edge[0])
+                Loosing_end.append(edge[1])
+            
+        Dominating_end,Loosing_end = list(set(Dominating_end) - set(Loosing_end)), list(set(Loosing_end) - set(Dominating_end))
+
+        return Dominating_end[0] if len(Dominating_end)==1 else -1
+
+
+        
